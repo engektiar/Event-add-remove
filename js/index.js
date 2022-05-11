b5fe7c9edf34584102ff6b96329d397f4caeac35
Ektiar <eng.ektiar@gmail.com>
2022-05-11 22:38:27 +0600

    javascrip style add

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var mypara = document.getElementById("para");
+function paraAdd() {
+  mypara.classList.add("paraStyle");
+}
+function paraReame() {
+  mypara.classList.remove("paraStyle");
+}
+var aboutmy = document.getElementById("ek");
+function ekadd() {
+  aboutmy.classList.add("about");
+}
+
+function ekreamove() {
+  aboutmy.classList.remove("about");
+}
